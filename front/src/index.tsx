d125f8b41dd469a944b9964ba8157018171c6982
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-02-18 16:43:57 +0900

    style: front files

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import './index.css'
+import App from './app/App'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import store from './app/store'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// react-query全体での初期設定
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+})
+
+const container = document.getElementById('app')
+const root = createRoot(container!) // createRoot(container!) if you use TypeScript
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
