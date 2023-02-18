839a3a95db29b29a99f4034d835cacfe3e20c730
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-02-18 12:28:17 +0900

    feat: create User seeder for logging in with google

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "test example",
+            'email' => "test.example.test.example.test@gmail.com",
+            'password' => Hash::make("password"),
+        ]);
+    }
+}
