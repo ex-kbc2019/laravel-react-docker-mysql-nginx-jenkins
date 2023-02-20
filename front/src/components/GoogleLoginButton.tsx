e9b40b3167133087a91e822f801c13c5ab40ddc3
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-02-20 22:35:42 +0900

    style: format GoogleLoginButton

--- front/src/components/GoogleLoginButton.tsx
+++ front/src/components/GoogleLoginButton.tsx
@@ -1,12 +1,10 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const GoogleLoginButton = () => {
-    const onClick = () => {
-        axios.get("/api/login/google").then((res) => {
-            window.location.href = res.data.redirect_url;
-        });
-    };
-    return (
-        <button onClick={onClick}>google login</button>
-    );
-};
+  const onClick = () => {
+    axios.get('/api/login/google').then((res) => {
+      window.location.href = res.data.redirect_url
+    })
+  }
+  return <button onClick={onClick}>google login</button>
+}
